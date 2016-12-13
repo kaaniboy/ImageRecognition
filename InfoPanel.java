6497f9ac839c3399e445c2b6a26a4c148cbd6893
kaaniboy <kaanaksoyaz@gmail.com>
2016-12-12 18:21:18 -0700

    Using JAMA matrix library, restructured weights matrices

--- InfoPanel.java
+++ InfoPanel.java
@@ -2,14 +2,18 @@ import java.util.Arrays;
 
 import javax.swing.JPanel;
 
+import Jama.Matrix;
+
 public class InfoPanel extends JPanel {
 	NeuralNetwork net;
 
 	public InfoPanel() {
 		net = new NeuralNetwork();
-		System.out.println(net);
-		System.out.println("Hidden layer results:");
-		System.out.println(Arrays.toString(net.run(new double[] { 1, 1, .5, .25, 1 })));
+		net.print();
+		/* System.out.println("Hidden layer results:"); */
+		Matrix input = new Matrix(new double[][] { { 1, 1, .5, .25, 1 } }).transpose();
+		System.out.println("\nOutput:\n");
+		net.run(input).print(5, 2);
 	}
 
 }
--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -1,13 +1,17 @@
 import java.text.DecimalFormat;
 import java.util.Random;
 
+import Jama.Matrix;
+
 public class NeuralNetwork {
 	private static int INPUT_SIZE = 5;
 	private static int HIDDEN_SIZE = 3;
 	private static int OUTPUT_SIZE = 4;
 	
-	private double[][] weights1 = new double[INPUT_SIZE][HIDDEN_SIZE];
-	private double[][] weights2 = new double[HIDDEN_SIZE][OUTPUT_SIZE];
+	private Matrix weights1;
+	private Matrix weights2;
+	private double[][] w1 = new double[HIDDEN_SIZE][INPUT_SIZE];
+	private double[][] w2 = new double[OUTPUT_SIZE][HIDDEN_SIZE];
 	
 	public NeuralNetwork() {
 		initializeRandom();
@@ -15,69 +19,68 @@ public class NeuralNetwork {
 	
 	private void initializeRandom() {
 		Random rand = new Random();
-		for(int i = 0; i < INPUT_SIZE; i++) {
-			for(int j = 0; j < HIDDEN_SIZE; j++) {
-				weights1[i][j] = rand.nextDouble() * 2 - 1;
-			}
-		}
-		
-		for(int i = 0; i < HIDDEN_SIZE; i++) {
-			for(int j = 0; j < OUTPUT_SIZE; j++) {
-				weights2[i][j] = rand.nextDouble() * 2 - 1;
-			}
-		}
-	}
-	
-	public double[] run(double[] input) {
-		if(input.length != INPUT_SIZE) throw new IllegalArgumentException();
-		
-		
-		double[] hidden = new double[HIDDEN_SIZE];
-		
 		for(int i = 0; i < HIDDEN_SIZE; i++) {
 			for(int j = 0; j < INPUT_SIZE; j++) {
-				hidden[i] += input[j] * weights1[j][i];
+				w1[i][j] = rand.nextDouble() * 2 - 1;
 			}
-			hidden[i] = sigmoid(hidden[i]);
 		}
 		
-		double[] output = new double[OUTPUT_SIZE];
+		weights1 = new Matrix(w1);
 		
 		for(int i = 0; i < OUTPUT_SIZE; i++) {
 			for(int j = 0; j < HIDDEN_SIZE; j++) {
-				output[i] += hidden[j] * weights2[j][i];
+				w2[i][j] = rand.nextDouble() * 2 - 1;
 			}
-			output[i] = sigmoid(output[i]);
 		}
-		return output;
+		
+		weights2 = new Matrix(w2);
 	}
 	
+	public Matrix run(Matrix input) {
+		if(input.getColumnDimension() != 1 || input.getRowDimension() != INPUT_SIZE) {
+			throw new IllegalArgumentException();
+		}
+		Matrix hiddenActivations = weights1.times(input);
+		elementwiseSigmoid(hiddenActivations);
+		
+		Matrix outputActivations = weights2.times(hiddenActivations);
+		elementwiseSigmoid(outputActivations);
+		
+		return outputActivations;
+	}
+	
+	private void elementwiseSigmoid(Matrix mat) {
+		for(int i = 0; i < mat.getRowDimension(); i++) {
+			for(int j = 0; j < mat.getColumnDimension(); j++) {
+				mat.set(i, j, sigmoid(mat.get(i, 0)));
+			}
+		}
+	}
 	private double sigmoid(double x) {
 		return 1/(1 + Math.pow(Math.E, -1 * x));
 	}
 	
-	@Override
-	public String toString() {
-		DecimalFormat df = new DecimalFormat("0.00");
-		StringBuilder sb = new StringBuilder();
-		sb.append("Input layer to hidden layer:\n\n");
+	public void print() {
+		System.out.println("Input layer to hidden layer:\n\n");
 		
-		for(int i = 0; i < INPUT_SIZE; i++) {
+		weights1.print(5, 2);
+		
+		/*for(int i = 0; i < INPUT_SIZE; i++) {
 			for(int j = 0; j < HIDDEN_SIZE; j++) {
-				sb.append(df.format(weights1[i][j]) + " ");
+				sb.append(df.format(w1[i][j]) + " ");
 			}
 			sb.append("\n");
-		}
+		}*/
 		
-		sb.append("\nHidden layer to output layer:\n\n");
+		System.out.println("\nHidden layer to output layer:\n\n");
 		
-		for(int i = 0; i < HIDDEN_SIZE; i++) {
+		weights2.print(5, 2);
+		
+		/*for(int i = 0; i < HIDDEN_SIZE; i++) {
 			for(int j = 0; j < OUTPUT_SIZE; j++) {
 				sb.append(df.format(weights2[i][j]) + " ");
 			}
 			sb.append("\n");
-		}
-		
-		return sb.toString();
+		}*/
 	}
 }
